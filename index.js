15549b952ab455cb0cf1ae84814a3bec4b97982a
Ishaan Bhatia <ishaanbhatiawork@gmail.com>
2021-03-03 18:36:31 +0530

    First commit for the MEN Backend.

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const mongoose = require("mongoose");
+
+const app = express();
+
+const apiRoutes = require("./api-routes");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost/resthub", { useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+if (!db) {
+  console.log(`Error connecting DB`);
+} else {
+  console.log("DB connected");
+}
+
+const port = process.env.PORT || 8080;
+
+app.get("/", (req, res) => res.send("Hello world with Express and Nodemon"));
+
+app.use("/api", apiRoutes);
+
+app.listen(port, () => {
+  console.log(`Running RestHub on ${port} `);
+});
